bb8596e115bd7d9ed7f8da287535284689c7aa54
MrXandbadas <kodiea@gmail.com>
2020-12-24 16:49:14 +1100

    views to pages
    virews to change

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../pages/Home.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -13,27 +13,27 @@ const routes: Array<RouteRecordRaw> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../pages/About.vue')
   },
   {
     path: '/hire',
     name: 'hire',
-    component: () => import('../views/content2020/Hire.vue')
+    component: () => import('../pages/content2020/Hire.vue')
   },
   {
     path: '/lighting',
     name: 'lighting',
-    component: () => import('../views/content2020/Lighting.vue')
+    component: () => import('../pages/content2020/Lighting.vue')
   },
   {
     path: '/services',
     name: 'services',
-    component: () => import('../views/content2020/Services.vue')
+    component: () => import('../pages/content2020/Services.vue')
   },
   {
     path: '/sfx',
     name: 'sfx',
-    component: () => import('../views/content2020/Sfx.vue')
+    component: () => import('../pages/content2020/Sfx.vue')
   }
 ]
 
