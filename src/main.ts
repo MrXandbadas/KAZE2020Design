48ea4e29d81cf61169b046e2de855412cd807357
MrXandbadas <kodiea@gmail.com>
2020-12-28 17:36:17 +1100

    global CSS started
    globallll

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import 'vue-lazy/dist/vue-lazy.css'
 
+import "./assets/styles/global.css"
 import "./assets/styles/main.css";
 import "./assets/styles/components/footer.css";
 import "./assets/styles/components/header.css";
